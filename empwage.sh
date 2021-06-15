250fc96cb0634b1065cc81869faf2faf9ada6487
kajal-12-eng <kajalsingh85654@gamil.com>
2021-06-15 14:23:05 +0530

    employee attendance

--- empwage.sh
+++ empwage.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 echo "welcome to employee wage problem "
+ispresent=1
+randomcheck=$((RANDOM%2))
+if [ $ispresent -eq $randomcheck))
+then
+    echo "employee is present"
+else
+    echo "employee is absent"
+fi
 
